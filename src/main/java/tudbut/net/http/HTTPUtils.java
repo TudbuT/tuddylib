0ec9bca859286e0f42cd68e675694c543c154709
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-05-22 19:02:39 +0200

    - Added HTTPUtils#createRateLimitingHandler

--- src/main/java/tudbut/net/http/HTTPUtils.java
+++ src/main/java/tudbut/net/http/HTTPUtils.java
@@ -1,9 +1,17 @@
 package tudbut.net.http;
 
+import tudbut.obj.DoubleObject;
+import tudbut.obj.DoubleTypedObject;
+import tudbut.obj.TLMap;
+import tudbut.tools.Lock;
+
 import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 /**
  * Utils for sending/receiving with HTTP
@@ -32,5 +40,45 @@ public class HTTPUtils {
     
     public static String utf8ToRaw(String utf8) {
         return new String(utf8.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+    }
+    
+    public static HTTPServer.HTTPHandler createRateLimitingHandler(int rps, HTTPServer.HTTPHandler requestHandler) {
+        TLMap<String, DoubleTypedObject<Integer, Lock>> map = new TLMap<>();
+        return new HTTPServer.HTTPHandler() {
+            @Override
+            public void handle(HTTPServerRequest request) throws Exception {
+                requestHandler.handle(request);
+            }
+    
+            @Override
+            public boolean accept(SocketAddress address) {
+                String ip = ((InetSocketAddress) address).getHostString();
+                DoubleTypedObject<Integer, Lock> user = map.get(ip);
+                if(user == null) {
+                    map.set(ip, user = new DoubleTypedObject<>(0, new Lock()));
+                }
+                user.o++;
+                if (user.t.isLocked()) {
+                    if (user.o > rps) {
+                        return false;
+                    }
+                }
+                else {
+                    user.t.lock(1000);
+                    user.o = 1;
+                }
+                return requestHandler.accept(address);
+            }
+    
+            @Override
+            public void handleDeny(HTTPServerRequest request) throws Exception {
+                requestHandler.handleDeny(request);
+            }
+    
+            @Override
+            public HTTPResponse onError(HTTPServerRequest request, HTTPResponse defaultResponse, Throwable theError) throws Exception {
+                return requestHandler.onError(request, defaultResponse, theError);
+            }
+        };
     }
 }
