39107a552b0bc1b42af96417cd0bc4a89a671b46
TudbuT <tudbut@tudbut.de>
2022-07-10 17:31:57 +0200

    add Nullable (its basically a monad)

---
+++ src/main/java/de/tudbut/tools/Nullable.java
@@ -0,0 +1,48 @@
+package de.tudbut.tools;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Nullable<T> {
+
+    public T object;
+
+    public Nullable(T object) {
+        this.object = object;
+    }
+
+    public <R> Nullable<R> ensure(Function<T, R> func) {
+        if(object == null)
+            throw new NullPointerException();
+        return new Nullable<R>(func.apply(object));
+    }
+
+    public <R> Nullable<R> apply(Function<T, R> func) {
+        if(object == null)
+            return new Nullable<R>(null);
+        return new Nullable<R>(func.apply(object));
+    }
+
+    public void ensureConsume(Consumer<T> func) {
+        if(object == null)
+            throw new NullPointerException();
+        func.accept(object);
+    }
+
+    public void consume(Consumer<T> func) {
+        if(object == null)
+            return;
+        func.accept(object);
+    }
+
+    public T ensureGet() {
+        if(object == null)
+            throw new NullPointerException();
+        return object;
+    }
+
+    public T get() {
+        return object;
+    }
+
+}
